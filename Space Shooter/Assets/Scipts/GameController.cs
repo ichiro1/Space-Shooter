51fa837dbc5208928a2003506b082a221735eb9c
UCode <UCode@UCODE-TPV-GAMMA.fios-router.home>
2018-08-22 16:51:29 -0700

    Refinished Game

--- Space Shooter/Assets/Scipts/GameController.cs
+++ Space Shooter/Assets/Scipts/GameController.cs
@@ -12,13 +12,31 @@ public class GameController : MonoBehaviour
     public float waveWait;
     
     public Text scoreText;
-    public int score;
+    public Text restartText;
+    public Text gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
     
     void Start ()
     {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
         score = 0;
         UpdateScore ();
         StartCoroutine (SpawnWaves ());
+    }
+
+    void Update() {
+        if(restart) {
+            if(Input.GetKeyDown (KeyCode.R)) {
+                Application.LoadLevel (Application.loadedLevel);
+            }
+        }
+
     }
     
     IEnumerator SpawnWaves ()
@@ -32,6 +50,12 @@ public class GameController : MonoBehaviour
                 yield return new WaitForSeconds (spawnWait);
             }
             yield return new WaitForSeconds (waveWait);
+
+            if(gameOver) {
+                restartText.text = "Press 'R' to Restart";
+                restart = true;
+                break;
+            }
         }
     }
     public void AddScore (int newScoreValue) {
@@ -41,5 +65,11 @@ public class GameController : MonoBehaviour
     
     void UpdateScore () {
         scoreText.text = "Score: " + score;
+    }
+    
+    public void GameOver() {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+
     }
 }
